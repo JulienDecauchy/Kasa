a9352ee68ffbb3a315dad6968d524cda42bf1992
JulienDecauchy <julien.decauchy1@gmail.com>
2023-08-01 11:35:31 +0200

    Update Router
    - Added NavBar.js + Components directory

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import "./App.css";
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Accueil from "../src/Pages/page_accueil";
 import Apropos from "../src/Pages/page_apropos";
 import Logement from "../src/Pages/page_logement";
 import Erreur404 from "../src/Pages/page_erreur404";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
-    <div className="container">
-      <nav>
-        <ul>
-          <Link to="/" class="list">
-            Accueil
-          </Link>
-          <Link to="/apropos" class="list">
-            A Propos
-          </Link>
-          <Link to="/logement" class="list">
-            Logement
-          </Link>
-        </ul>
-      </nav>
-
+    <>
+      <NavBar />
       {/* Defining routes path and rendering components as element */}
       <Routes>
         <Route path="/" element={<Accueil />} />
@@ -31,7 +19,7 @@ function App() {
         <Route path="/logement" element={<Logement />} />
         <Route path="*" element={<Erreur404 />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
